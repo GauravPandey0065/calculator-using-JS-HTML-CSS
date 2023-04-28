8398de624c629f4e96529eda0df0e6f407aa6fd9
gaurav pandey <127774454+GauravPandey0065@users.noreply.github.com>
2023-04-28 11:48:36 +0530

    Update CALCULATOR-JS.js

--- CALCULATOR-JS.js
+++ CALCULATOR-JS.js
@@ -1,44 +1,19 @@
-(function () {
-
-
-    let screen = document.querySelector('.screen');
-
-    let buttons = document.querySelectorAll('.btn');
-
-    let equal = document.querySelector('.btn-equal');
-
-    let clear = document.querySelector('.btn-clear');
-
-
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-
-
-        })
-
-
-    });
-
-    equal.addEventListener('click', function (e) {
-        if (screen.value == '') {
-
-            screen.value = "";
-        } else {
-            let answer = eval(screen.value);
-            screen.value = answer;
+let string = "";
+let buttons = document.querySelectorAll('.buttons');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
         }
-
-    });
-
-
-    clear.addEventListener('click', function (e) {
-
-        screen.value = "";
-
-
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
     })
-
-
-})();
+})
